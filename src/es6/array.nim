3ef751d6297f2a6e0c1d2ac3284f446de7722011
Gary Becks <techstar.dev@hotmail.com>
2020-10-30 16:27:19 -0400

    add length and concat

--- src/es6/array.nim
+++ src/es6/array.nim
@@ -1,24 +1,19 @@
-import jsffi
-
-
-var console {.importc, nodecl.}: JsObject
-
 type JsArray*[T] = ref object
 
-proc length(self: JsArray): int {.
+proc length*(self: JsArray): int {.
     importcpp: "#.length", noSideEffect.}
-proc concat[T](self: JsArray[T], arrays: varargs[JsArray[T]]): JsArray[T] {.
-    importcpp: "Array.from(#)", noSideEffect.}
-
-
-proc jsArray[T](self: JsArray[T], items: varargs[T]): JsArray[T] {.
+proc concat*[T](self: JsArray[T], arrays: varargs[JsArray[T]]): JsArray[T] {.
     importcpp: "#.concat(#)", noSideEffect.}
 
-proc map[T, U](self: JsArray[T], callback: proc(val: T): U): JsArray[U] {.
+
+proc jsArray*[T](items: varargs[T]): JsArray[T] {.
+    importcpp: "Array.from(#)", noSideEffect.}
+
+proc map*[T, U](self: JsArray[T], callback: proc(val: T): U): JsArray[U] {.
     importcpp: "#.map(#)", noSideEffect.}
 
-proc map[T, U](self: JsArray[T], callback: proc(val: T,
+proc map*[T, U](self: JsArray[T], callback: proc(val: T,
     index: int): U): JsArray[U] {.importcpp: "#.map(#)", noSideEffect.}
 
-proc map[T, U](self: JsArray[T], callback: proc(val: T, index: int,
+proc map*[T, U](self: JsArray[T], callback: proc(val: T, index: int,
     array: JsArray[T]): U): JsArray[U] {.importcpp: "#.map(#)", noSideEffect.}
