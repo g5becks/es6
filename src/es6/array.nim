7ca432646da962ba2319a45b150aa13a580415e9
Gary Becks <techstar.dev@hotmail.com>
2020-10-30 16:22:14 -0400

    initial

---
+++ src/es6/array.nim
@@ -0,0 +1,24 @@
+import jsffi
+
+
+var console {.importc, nodecl.}: JsObject
+
+type JsArray*[T] = ref object
+
+proc length(self: JsArray): int {.
+    importcpp: "#.length", noSideEffect.}
+proc concat[T](self: JsArray[T], arrays: varargs[JsArray[T]]): JsArray[T] {.
+    importcpp: "Array.from(#)", noSideEffect.}
+
+
+proc jsArray[T](self: JsArray[T], items: varargs[T]): JsArray[T] {.
+    importcpp: "#.concat(#)", noSideEffect.}
+
+proc map[T, U](self: JsArray[T], callback: proc(val: T): U): JsArray[U] {.
+    importcpp: "#.map(#)", noSideEffect.}
+
+proc map[T, U](self: JsArray[T], callback: proc(val: T,
+    index: int): U): JsArray[U] {.importcpp: "#.map(#)", noSideEffect.}
+
+proc map[T, U](self: JsArray[T], callback: proc(val: T, index: int,
+    array: JsArray[T]): U): JsArray[U] {.importcpp: "#.map(#)", noSideEffect.}
